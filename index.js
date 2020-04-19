6981ac27a255ad273b91bd3a29a1594835a71ea3
Paul <prose0021@gmail.com>
2020-04-19 15:48:28 -0700

    config

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import "bootstrap/dist/css/bootstrap.css";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<p>working</p>, root);
+})
