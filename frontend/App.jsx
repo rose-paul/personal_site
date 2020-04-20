3c997915d28a56894d7580ea4c8d64970ba20fe8
Paul <prose0021@gmail.com>
2020-04-20 09:06:49 -0700

    bootstrap

---
+++ frontend/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const App = () => {
+   
+    const [component, setComponent] = useState(<Home />)
+
+    function changeTab(num) {
+        switch (num) {
+            case 1:
+                return setComponent(<Home />)
+            case 2: 
+                return setComponent(<About />)
+            case 3: 
+                return setComponent(<Portfolio />)
+            default:
+                return
+        }
+    }
+   
+   return ( <>
+        <NavBar changeTab={changeTab} />
+        {component}
+    </>)
+}
+
+export default App;
