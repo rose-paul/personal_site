f76eccdc1e42010822af592aea7f9190ab9f8757
Paul <prose0021@gmail.com>
2020-05-20 14:44:20 -0700

    change bio

--- frontend/about.jsx
+++ frontend/about.jsx
@@ -7,17 +7,21 @@ const About = () => (
     <section className="about">
       <h2>About Me</h2>
       <article>
-        I came into the tech realm as an engineer following my previous work
-        teaching at tech companies in Moscow. I enjoy all aspects of fullstack development, 
-        from page design to system design. My most recent endeavors have focused on 
-        engineering in JavaScript, React and AWS.
+      My previous work teaching at tech companies in Moscow introduced me to the spellbinding realm of software
+       development: the challenge of problem-solving and designing systems, the fun of building, and the potential for making 
+       impactful products and experiences.
+      </article>
+      <article>
+        Fast-forward from that initial enchantment to now: I have several 
+        engineering projects using a range of technologies. 
+        I’m especially adept at JavaScript, React and the surrounding eco-system. 
       </article>
 
       <article>
         Outside of programming, I love to travel. I was fortunate to explore a
         lot of places during my time in Moscow, both in the country and the
         surrounding region. This photo is from the Krasnodar region on top of
-        the mountain Indyuk. Feel free to reach out if you want to chat more!
+        the mountain Indyuk.
       </article>
     </section>
         <Facts />
