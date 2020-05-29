373f9c8478382fcb96017f1af38df7e6b6c6ff83
Paul <prose0021@gmail.com>
2020-05-29 14:40:09 -0700

    carousel

--- frontend/carousel.jsx
+++ frontend/carousel.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const Carousel = () => {
 
-    const facts = ["Paul van Dyk Fanatic", "React Buff", "Pelmeni Enthusiast", "JavaScript Obsessed"]
+    const facts = ["Paul van Dyk Fanatic", "React Buff", "Pelmeni Enthusiast", "Jazzed by JS"]
     const [display, setDisplay] = useState("Paul van Dyk Fanatic");
     const len = facts.length;
     let index = 1;
